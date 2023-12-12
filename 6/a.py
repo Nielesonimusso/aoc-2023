4a2e30c8557c718d7d423f3f5d9f6304ce0df5a8
Niels Rood <n.rood@avular.com>
2023-12-12 21:26:49 +0100

    day 6

---
+++ 6/a.py
@@ -0,0 +1,24 @@
+import re
+from math import sqrt, floor
+
+numbers_regex = re.compile(r'\d+')
+
+total = 1
+
+with open("data.txt", "r") as file:
+    times = [int(n) for n in numbers_regex.findall(file.readline())]
+    records = [int(n) for n in numbers_regex.findall(file.readline())]
+
+    for time, record in zip(times, records):
+        D = time * time - 4 * record
+        
+        hold_start = (time - sqrt(D))/2
+        hold_end = (time + sqrt(D))/2
+        
+        hold_range = range(floor(hold_start) + 1, floor(hold_end) + 1)
+        number_of_ways = len(hold_range)
+        print(f"for time {time} should hold within range {hold_range} ({number_of_ways} options)")
+
+        total *= number_of_ways
+
+print(total)
